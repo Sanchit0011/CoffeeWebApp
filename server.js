6b89ea8a94489a6f86d0d2b456317cd701ec174f
sanchit <sanchitsh0211@gmail.com>
2019-04-21 16:10:16 +1000

    Added staff functionality

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Load required modules and functionality
-const express = require('express')
-const db = require('./queries')
+import express, { json } from 'express';
+import db from './queries';
 const app = express()
-app.use(express.json())
+app.use(json())
 
 // Sent get request to retrieve cakes and sandwiches
 app.get('/api/cakesandwich', db.readCakeSandwich)
 
-// Sent get request to retrieve cakes and sandwiches by id
-app.get('/api/cakesandwich/:id', db.readCakeSandwichByID)
+// Sent get request to retrieve staff information
+app.get('/api/staff', db.readStaff)
 
 // Sent post request to store order information
 app.post('/api/orders', db.addOrder)
@@ -16,14 +16,20 @@ app.post('/api/orders', db.addOrder)
 // Sent post request to store cake/sandwich information
 app.post('/api/cakesandwich', db.addCakeSandwich)
 
+// Sent post request to add staff member to system 
+app.post('/api/staff', db.addStaff)
+
 // Sent put request to add time taken to complete order 
 app.put('/api/orders/:id', db.addTime)
 
 // Sent put request to update cake/sandwich status
-app.put('/api/cakesandwich/:id', db.updateCakeSandwichStatus)
+app.put('/api/cakesandwich/:name', db.updateCakeSandwichStatus)
 
 // Sent delete request to delete cake/sandwich
-app.delete('/api/cakesandwich/:id', db.delCakeSandwich)
+app.delete('/api/cakesandwich/:name', db.delCakeSandwich)
+
+// Sent delete request to delete staff member from system
+app.delete('/api/staff/:id', db.delStaff)
 
 // Listening on defined port
 const port = process.env.PORT || 3000
