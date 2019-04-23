d0dbd523aaec70707112749e6b0a02e55098496b
sanchit <sanchitsh0211@gmail.com>
2019-04-23 16:34:03 +1000

    Added minor changes

--- queries.js
+++ queries.js
@@ -1,10 +1,12 @@
 // Establishing connection with postgresql
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'SanSak12$',
-    port: 5432
+    user: 'ygjpicmywavcnj',
+    host: 'ec2-50-19-127-115.compute-1.amazonaws.com',
+    database: 'dauqu68lisig93',
+    password: '1c8c280cf46ba300954c41c637ba4705b24462d400b67d61c1be8155b11db9aa',
+    port: 5432,
+    max: 15,
+    ssl: true
 })
 
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// Load required modules and functionality
 const express = require('express')
+const db = require('./queries');
 const app = express()
-const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Listening on defined port
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
